7548793c168050f03068f16ee7c19f8159132777
David Kim <rtdk0324@gmail.com>
2019-04-30 16:11:52 -0400

    Updates

---
+++ precache-manifest.b2a69f27d478cb675542d199409b0842.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d433f9c1174493fa35466e25a6d08bb",
+    "url": "/fivie/index.html"
+  },
+  {
+    "revision": "e9a8765b1a3cc863121c",
+    "url": "/fivie/static/css/main.03fb6801.chunk.css"
+  },
+  {
+    "revision": "35eada50e0f467f37b1c",
+    "url": "/fivie/static/js/2.3b397183.chunk.js"
+  },
+  {
+    "revision": "e9a8765b1a3cc863121c",
+    "url": "/fivie/static/js/main.7888c2df.chunk.js"
+  },
+  {
+    "revision": "2a3dbef340e5a231f99b",
+    "url": "/fivie/static/js/runtime~main.901bfddb.js"
+  },
+  {
+    "revision": "42d79eba974955740a95cc0fb71ca247",
+    "url": "/fivie/static/media/SFProDisplay-Bold.42d79eba.ttf"
+  },
+  {
+    "revision": "bfddc2a3b6bc31c9c25a8f391450dd00",
+    "url": "/fivie/static/media/SFProDisplay-SemiBold.bfddc2a3.ttf"
+  },
+  {
+    "revision": "d6079ef01292c4bc84dce33988641530",
+    "url": "/fivie/static/media/SFProText-Bold.d6079ef0.ttf"
+  },
+  {
+    "revision": "a260cbc18870da144038776461d9df28",
+    "url": "/fivie/static/media/SFProText-Medium.a260cbc1.ttf"
+  },
+  {
+    "revision": "85bd46c1cff02c1d8360cc714b8298fa",
+    "url": "/fivie/static/media/SFProText-Regular.85bd46c1.ttf"
+  },
+  {
+    "revision": "9c11e40905e2c0d3ee22dea625a3a497",
+    "url": "/fivie/static/media/bg1.9c11e409.png"
+  },
+  {
+    "revision": "de132a92d9df2c4cb4328db767de50e0",
+    "url": "/fivie/static/media/bg2.de132a92.png"
+  },
+  {
+    "revision": "7fbaf307408c941f841b99289d9deb82",
+    "url": "/fivie/static/media/bg3.7fbaf307.png"
+  },
+  {
+    "revision": "5fc62e5a6e32c60001bb92aaa2d17cc6",
+    "url": "/fivie/static/media/bg4.5fc62e5a.png"
+  }
+]);
